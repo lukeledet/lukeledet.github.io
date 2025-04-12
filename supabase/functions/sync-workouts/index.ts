dafb1e4e12a6cc6f31454177084d113c3a8ee015
Luke Ledet <luke@revelry.co>
2025-04-12 11:00:54 -0400

    chore: use jsr instead of esm.sh

--- supabase/functions/sync-workouts/index.ts
+++ supabase/functions/sync-workouts/index.ts
@@ -1,5 +1,5 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from 'jsr:@supabase/supabase-js@2'
 import { corsHeaders } from '../_shared/cors.ts';
 const CONCEPT2_TOKEN_URL = 'https://log.concept2.com/oauth/access_token';
 const CONCEPT2_API_URL = 'https://log.concept2.com/api';
